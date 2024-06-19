b65321996205e552df69334bf79918b3e2d954b4
114corn <163351509+114corn@users.noreply.github.com>
2024-06-19 03:06:16 +0200

    improve variables

--- app.py
+++ app.py
@@ -8,49 +8,47 @@ load_dotenv()
 
 app = Flask(__name__)
 
-scheduled_tasks = {}
+tasks_schedule = {}
 
-def task_scheduler():
+def run_scheduled_tasks():
     while True:
-        to_remove = []
-        for task_id in scheduled_tasks.keys():
-            task = scheduled_tasks[task_id]
-            if task['execute']:
+        completed_tasks = []
+        for task_id, task_details in tasks_schedule.items():
+            if task_details['is_scheduled']:
                 try:
-                    task['function']()  
-                    task['execute'] = False  
+                    task_details['task_function']()  
+                    task_details['is_scheduled'] = False  
                 finally:
-                    to_remove.append(task_id)
-        for task_id in to_remove:
-            del scheduled_tasks[task_id]
+                    completed_tasks.append(task_id)
+        for task_id in completed_tasks:
+            del tasks_schedule[task_id]
         sleep(60)  
 
-def example_task():
-    print("Task executed")
+def perform_example_task():
+    print("Example task executed")
 
 @app.route('/schedule_task', methods=['POST'])
-def schedule_task():
-    data = request.get_json(force=True, silent=True)
-    if not data:
+def handle_task_scheduling():
+    request_data = request.get_json(force=True, silent=True)
+    if not request_data:
         return jsonify({"message": "Invalid or missing JSON in request"}), 400
 
-    task_id = data.get('task_id')
-    task_name = data.get('task_name')
+    task_id = request_data.get('task_id')
+    task_name = request_data.get('task_name')
 
-    # Validate task_id and task_name presence
     if not task_id or not task_name:
         return jsonify({"message": "Missing task_id or task_name"}), 400
 
-    if task_id in scheduled_tasks:
+    if task_id in tasks_schedule:
         return jsonify({"message": "Task with this ID is already scheduled", "task_id": task_id}), 400
 
     if task_name == "example_task":
-        scheduled_tasks[task_id] = {'function': example_task, 'execute': True}
-        return jsonify({"message": "Task scheduled", "task_id": task_id}), 200
+        tasks_schedule[task_id] = {'task_function': perform_example_task, 'is_scheduled': True}
+        return jsonify({"message": "Task successfully scheduled", "task_id": task_id}), 200
     else:
-        return jsonify({"message": "Task not found", "task_id": task_id}), 404
+        return jsonify({"message": "Specified task not found", "task_id": task_id}), 404
 
 if __name__ == '__main__':
-    scheduler_thread = Thread(target=task_scheduler, daemon=True)
-    scheduler_thread.start()
+    task_scheduler_thread = Thread(target=run_scheduled_tasks, daemon=True)
+    task_scheduler_thread.start()
     app.run(host=os.environ.get("HOST", "127.0.0.1"), port=int(os.environ.get("PORT", 5000)))
