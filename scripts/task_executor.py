755a503411571d270290982988b3f2c017862e2c
114corn <163351509+114corn@users.noreply.github.com>
2024-07-12 12:19:39 +0200

    optimization enhancement

--- scripts/task_executor.py
+++ scripts/task_executor.py
@@ -1,21 +1,22 @@
 import schedule
 import time
 import subprocess
-import smtplib, ssl
+import smtplib
+import ssl
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 import os
-from dotenv import load_dotenv
-from functools import lru_cache
 
 load_dotenv()
+SENDER_EMAIL = os.getenv('SENDER_EMAIL')
+RECEIVER_EMAIL = os.getenv('RETEIVER_EMAIL')
+EMAIL_PASSWORD = os.getenv('EMAIL_PASSWORD')
+SMTP_SERVER = os.getenv('SMTP_SERVER')
+SMTP_PORT = os.getenv('SMTP_PORT')
 
-@lru_cache(maxsize=None)
-def send_email():
-    sender_email = os.getenv('SENDER_EMAIL')
-    receiver_email = os.getenv('RECEIVER_EMAIL')  # Fix variable name consistency
-    password = os.getenv('EMAIL_PASSWORD')  # Fix variable name consistency
+ssl_context = ssl.create_default_context()
 
+def send_email(sender_email=SENDER_EMAIL, receiver_email=RECEIVER_EMAIL, password=EMAIL_PASSWORD, smtp_server=SMTP_SERVER, smtp_port=SMTP_PORT):
     message = MIMEMultipart("alternative")
     message["Subject"] = "Scheduled Email"
     message["From"] = sender_email
@@ -38,24 +39,19 @@ def send_email():
 
     part1 = MIMEText(text, "plain")
     part2 = MIMEText(html, "html")
-
     message.attach(part1)
     message.attach(part2)
 
     try:
-        context = ssl.create_default_context()
-        with smtplib.SMTP_SSL(os.getenv('SMTP_SERVER'), int(os.getenv('SMTP_PORT')), context=context) as server:
+        with smtplib.SMTP_SSL(smtp_server, int(smtp_port), context=ssl_context) as server:
             server.login(sender_email, password)
-            server.sendmail(
-                sender_email, receiver_email, message.as_string()
-            )
+            server.sendmail(sender_email, receiver_email, message.as_string())
     except Exception as e:
         print("Error sending email: ", e)
 
-@lru_cache(maxsize=None)
-def run_script():
+def run_script(script_path='path/to/your/script.py'):
     try:
-        subprocess.run(['python', 'path/to/your/script.py'], check=True)
+        subprocess.run(['python', script_path], check=True)
     except subprocess.CalledProcessError as e:
         print("The script execution failed: ", e)
 
